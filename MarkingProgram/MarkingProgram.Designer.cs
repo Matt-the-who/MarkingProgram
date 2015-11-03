fc93a9359395e7e8421a5a0189d15d45ac165873
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-11-03 14:42:16 -0500

    Marking program

---
+++ MarkingProgram/MarkingProgram.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MarkingProgram
+{
+    partial class frmMarkingProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblMarkAsInt = new System.Windows.Forms.Label();
+            this.txtMarkAsLevel = new System.Windows.Forms.TextBox();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(72, 89);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblMarkAsInt
+            // 
+            this.lblMarkAsInt.AutoSize = true;
+            this.lblMarkAsInt.Location = new System.Drawing.Point(69, 158);
+            this.lblMarkAsInt.Name = "lblMarkAsInt";
+            this.lblMarkAsInt.Size = new System.Drawing.Size(0, 13);
+            this.lblMarkAsInt.TabIndex = 1;
+            // 
+            // txtMarkAsLevel
+            // 
+            this.txtMarkAsLevel.Location = new System.Drawing.Point(61, 54);
+            this.txtMarkAsLevel.Name = "txtMarkAsLevel";
+            this.txtMarkAsLevel.Size = new System.Drawing.Size(100, 20);
+            this.txtMarkAsLevel.TabIndex = 2;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(226, 24);
+            this.mnuStrip.TabIndex = 3;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmMarkingProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 237);
+            this.Controls.Add(this.txtMarkAsLevel);
+            this.Controls.Add(this.lblMarkAsInt);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmMarkingProgram";
+            this.Text = "Marking Program";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblMarkAsInt;
+        private System.Windows.Forms.TextBox txtMarkAsLevel;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ MarkingProgram/MarkingProgram.cs
@@ -0,0 +1,120 @@
+﻿/*
+Created by Matthew Ho
+Created on 11-03-15
+Created for ICS3U
+Assignment #4-04
+This program converts a level mark to a percentage mark.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarkingProgram
+{
+    public partial class frmMarkingProgram : Form
+    {
+
+        public int Conversion (string markAsLevel)
+        {
+            //this function converts level mark to integer mark
+            int integerMark;
+
+            if (markAsLevel == "4+")
+            {
+                integerMark = 95;
+            }
+            else if (markAsLevel == "4")
+            {
+                integerMark = 90;
+            }
+            else if (markAsLevel == "4-")
+            {
+                integerMark = 85;
+            }
+            else if (markAsLevel == "3+")
+            {
+                integerMark = 77;
+            }
+            else if (markAsLevel == "3")
+            {
+                integerMark = 75;
+            }
+            else if (markAsLevel == "3-")
+            {
+                integerMark = 72;
+            }
+            else if (markAsLevel == "2+")
+            {
+                integerMark = 67;
+            }
+            else if (markAsLevel == "2")
+            {
+                integerMark = 65;
+            }
+            else if (markAsLevel == "2-")
+            {
+                integerMark = 62;
+            }
+            else if (markAsLevel == "1+")
+            {
+                integerMark = 57;
+            }
+            else if (markAsLevel == "1")
+            {
+                integerMark = 55;
+            }
+            else if (markAsLevel == "1-")
+            {
+                integerMark = 52;
+            }
+            else if (markAsLevel == "R")
+            {
+                integerMark = 30;
+            }
+            else if (markAsLevel == "NE")
+            {
+                integerMark = 0;
+            }
+            else if (markAsLevel == "r")
+            {
+                integerMark = 30;
+            }
+            else if (markAsLevel == "ne")
+            {
+                integerMark = 0;
+            }
+            else integerMark = -1;
+
+            return  integerMark;
+        }
+
+        public frmMarkingProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string markAsLevel = this.txtMarkAsLevel.Text;
+            int percentageMark;
+
+
+            percentageMark = Conversion(markAsLevel);
+
+            this.lblMarkAsInt.Text = "Your % mark is " + percentageMark;
+        }
+    }
+}
